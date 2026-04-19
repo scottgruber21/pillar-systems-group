3b8132c751d9209082ae694451a42365d1a301a0
Scott Gruber <scottgruber21@gmail.com>
2026-04-19 11:35:08 -0400

    sitemap and favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  // Replace this with your actual live domain
+  site: 'https://www.pillarsystemsgroup.com', 
+  
+  integrations: [
+    sitemap()
+  ],
+  
   vite: {
     plugins: [tailwindcss()]
   }
